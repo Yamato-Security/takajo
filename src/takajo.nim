d846d1dda27935ae3a3c864bbcbe7fb034a2fc8a
Yamato Security <71482215+YamatoSecurity@users.noreply.github.com>
2023-06-01 11:20:18 +0900

    change indent from 2 spaces to 4

--- src/takajo.nim
+++ src/takajo.nim
@@ -8,123 +8,122 @@ import std/tables
 import takajopkg/submodule
 
 proc listUndetectedEvtxFiles(timeline: string, evtxDir: string,
-    columnName: system.string = "EvtxFile", quiet: bool = false,
-        output: string = ""): int =
+        columnName: system.string = "EvtxFile", quiet: bool = false,
+                output: string = ""): int =
 
-  if not quiet:
-    styledEcho(fgGreen, outputLogo())
+    if not quiet:
+        styledEcho(fgGreen, outputLogo())
 
-  let csvData: TableRef[string, seq[string]] = getHayabusaCsvData(timeline, columnName)
-  var fileLists: seq[string] = getTargetExtFileLists(evtxDir, ".evtx")
+    let csvData: TableRef[string, seq[string]] = getHayabusaCsvData(timeline, columnName)
+    var fileLists: seq[string] = getTargetExtFileLists(evtxDir, ".evtx")
 
-  var detectedPaths: seq[string] = csvData[columnName].map(getFileNameWithExt)
-  detectedPaths = deduplicate(detectedPaths)
+    var detectedPaths: seq[string] = csvData[columnName].map(getFileNameWithExt)
+    detectedPaths = deduplicate(detectedPaths)
 
-  let checkResult = getUnlistedSeq(fileLists, detectedPaths)
-  var outputStock: seq[string] = @[]
+    let checkResult = getUnlistedSeq(fileLists, detectedPaths)
+    var outputStock: seq[string] = @[]
 
-  echo "Finished. "
-  echo "---------------"
+    echo "Finished. "
+    echo "---------------"
 
-  if checkResult.len == 0:
-    echo "Great! No undetected evtx files were found."
-    echo ""
-  else:
-    echo "Undetected evtx file identified."
-    echo ""
-    var numberOfEvtxFiles = 0
-    for undetectedFile in checkResult:
-      outputStock.add(undetectedFile)
-      inc numberOfEvtxFiles
-    outputStock.add("")
-    let undetectedPercentage = (checkResult.len() / fileLists.len()) * 100
-    echo fmt"{ undetectedPercentage :.4}% of the evtx files did not have any detections."
-    echo fmt"Number of evtx files not detected: {numberOfEvtxFiles}"
-    echo ""
-  if output != "":
-    let f = open(output, fmWrite)
-    defer: f.close()
-    for line in outputStock:
-      f.writeLine(line)
-    echo fmt"Saved File {output}"
-    echo ""
-  else:
-    echo outputstock.join("\n")
-  discard
+    if checkResult.len == 0:
+        echo "Great! No undetected evtx files were found."
+        echo ""
+    else:
+        echo "Undetected evtx file identified."
+        echo ""
+        var numberOfEvtxFiles = 0
+        for undetectedFile in checkResult:
+            outputStock.add(undetectedFile)
+            inc numberOfEvtxFiles
+        outputStock.add("")
+        let undetectedPercentage = (checkResult.len() / fileLists.len()) * 100
+        echo fmt"{ undetectedPercentage :.4}% of the evtx files did not have any detections."
+        echo fmt"Number of evtx files not detected: {numberOfEvtxFiles}"
+        echo ""
+    if output != "":
+        let f = open(output, fmWrite)
+        defer: f.close()
+        for line in outputStock:
+            f.writeLine(line)
+        echo fmt"Saved File {output}"
+        echo ""
+    else:
+        echo outputstock.join("\n")
+    discard
 
 
 proc listUnusedRules(timeline: string, rulesDir: string,
-    columnName: string = "RuleFile", quiet: bool = false,
-        output: string = ""): int =
+        columnName: string = "RuleFile", quiet: bool = false,
+                output: string = ""): int =
 
-  if not quiet:
-    styledEcho(fgGreen, outputLogo())
+    if not quiet:
+        styledEcho(fgGreen, outputLogo())
 
-  let csvData: TableRef[string, seq[string]] = getHayabusaCsvData(timeline, columnName)
-  var fileLists: seq[string] = getTargetExtFileLists(rulesDir, ".yml")
-  var detectedPaths: seq[string] = csvData[columnName].map(getFileNameWithExt)
-  detectedPaths = deduplicate(detectedPaths)
-  var outputStock: seq[string] = @[]
+    let csvData: TableRef[string, seq[string]] = getHayabusaCsvData(timeline, columnName)
+    var fileLists: seq[string] = getTargetExtFileLists(rulesDir, ".yml")
+    var detectedPaths: seq[string] = csvData[columnName].map(getFileNameWithExt)
+    detectedPaths = deduplicate(detectedPaths)
+    var outputStock: seq[string] = @[]
 
-  echo "Finished. "
-  echo "---------------"
+    echo "Finished. "
+    echo "---------------"
 
-  let checkResult = getUnlistedSeq(fileLists, detectedPaths)
-  if checkResult.len == 0:
-    echo "Great! No unused rule files were found."
-    echo ""
-  else:
-    echo "Unused rule file identified."
-    echo ""
-    var numberOfUnusedRules = 0
-    for undetectedFile in checkResult:
-      outputStock.add(undetectedFile)
-      inc numberOfUnusedRules
-    let undetectedPercentage = (checkResult.len() / fileLists.len()) * 100
-    outputStock.add("")
-    echo fmt"{ undetectedPercentage :.4}% of the yml rules were not used."
-    echo fmt"Number of unused rule files: {numberOfUnusedRules}"
-    echo ""
-  if output != "":
-    let f = open(output, fmWrite)
-    defer: f.close()
-    for line in outputStock:
-      f.writeLine(line)
-    echo fmt"Saved File {output}"
-    echo ""
-  else:
-    echo outputstock.join("\n")
-  discard
+    let checkResult = getUnlistedSeq(fileLists, detectedPaths)
+    if checkResult.len == 0:
+        echo "Great! No unused rule files were found."
+        echo ""
+    else:
+        echo "Unused rule file identified."
+        echo ""
+        var numberOfUnusedRules = 0
+        for undetectedFile in checkResult:
+            outputStock.add(undetectedFile)
+            inc numberOfUnusedRules
+        let undetectedPercentage = (checkResult.len() / fileLists.len()) * 100
+        outputStock.add("")
+        echo fmt"{ undetectedPercentage :.4}% of the yml rules were not used."
+        echo fmt"Number of unused rule files: {numberOfUnusedRules}"
+        echo ""
+    if output != "":
+        let f = open(output, fmWrite)
+        defer: f.close()
+        for line in outputStock:
+            f.writeLine(line)
+        echo fmt"Saved File {output}"
+        echo ""
+    else:
+        echo outputstock.join("\n")
+    discard
 
 when isMainModule:
-  clCfg.version = "1.0.0-dev"
-  const examples = "Examples:\n  undetected-evtx -t ../hayabusa/timeline.csv -e ../hayabusa-sample-evtx\n  unused-rules -t ../hayabusa/timeline.csv -r ../hayabusa/rules\n"
-  clCfg.useMulti = "Usage: takajo.exe <COMMAND>\n\nCommands:\n$subcmds\nCommand help: $command help <COMMAND>\n\n" & examples
+    clCfg.version = "2.0.0-dev"
+    const examples = "Examples:\n  undetected-evtx -t ../hayabusa/timeline.csv -e ../hayabusa-sample-evtx\n  unused-rules -t ../hayabusa/timeline.csv -r ../hayabusa/rules\n"
+    clCfg.useMulti = "Usage: takajo.exe <COMMAND>\n\nCommands:\n$subcmds\nCommand help: $command help <COMMAND>\n\n" & examples
 
-  if paramCount() == 0:
-    styledEcho(fgGreen, outputLogo())
-  dispatchMulti(
-    [
-      listUndetectedEvtxFiles, cmdName = "undetected-evtx",
-      doc = "List up undetected evtx files",
-      help = {
-        "timeline": "CSV timeline created by Hayabusa with verbose profile",
-        "evtxDir": "The directory of .evtx files you scanned with Hayabusa",
-        "columnName": "Specify custom column header name",
-        "quiet": "Do not display the launch banner",
-        "output": "Save the results to a text file. The default is to print to screen.",
-      }
-    ],
-    [
-      listUnusedRules, cmdName = "unused-rules",
-      doc = "List up unused rules",
-      help = {
-        "timeline": "CSV timeline created by Hayabusa with verbose profile",
-        "rulesDir": "Hayabusa rules directory",
-        "columnName": "Specify custom column header name",
-        "quiet": "Do not display the launch banner",
-        "output": "Save the results to a text file. The default is to print to screen.",
-      }
-    ]
-  )
-
+    if paramCount() == 0:
+        styledEcho(fgGreen, outputLogo())
+    dispatchMulti(
+        [
+            listUndetectedEvtxFiles, cmdName = "undetected-evtx",
+            doc = "List up undetected evtx files",
+            help = {
+                "timeline": "CSV timeline created by Hayabusa with verbose profile",
+                "evtxDir": "The directory of .evtx files you scanned with Hayabusa",
+                "columnName": "Specify custom column header name",
+                "quiet": "Do not display the launch banner",
+                "output": "Save the results to a text file. The default is to print to screen.",
+            }
+        ],
+        [
+            listUnusedRules, cmdName = "unused-rules",
+            doc = "List up unused rules",
+            help = {
+                "timeline": "CSV timeline created by Hayabusa with verbose profile",
+                "rulesDir": "Hayabusa rules directory",
+                "columnName": "Specify custom column header name",
+                "quiet": "Do not display the launch banner",
+                "output": "Save the results to a text file. The default is to print to screen.",
+            }
+        ]
+    )
--- src/takajopkg/submodule.nim
+++ src/takajopkg/submodule.nim
@@ -7,69 +7,69 @@ import std/tables
 from std/streams import newFileStream
 
 proc outputLogo*(): string =
-  let logo = """
+    let logo = """
 ╔════╦═══╦╗╔═╦═══╗ ╔╦═══╗
 ║╔╗╔╗║╔═╗║║║╔╣╔═╗║ ║║╔═╗║
 ╚╝║║╚╣║ ║║╚╝╝║║ ║║ ║║║ ║║
   ║║ ║╚═╝║╔╗╖║╚═╝╠╗║║║ ║║
  ╔╝╚╗║╔═╗║║║╚╣╔═╗║╚╝║╚═╝║
  ╚══╝╚╝ ╚╩╝╚═╩╝ ╚╩══╩═══╝
-   by Yamato Security
+  by Yamato Security
 """
-  return logo
+    return logo
 
 proc getUnlistedSeq*(targetSeq: seq[string], compareSeq: seq[string]): seq[string] =
-  ## get element not in compareSeq
-  var output: seq[string] = @[]
-  for target in targetSeq:
-    if not(target in compareSeq):
-      output.add(target)
-  return output
+    ## get element not in compareSeq
+    var output: seq[string] = @[]
+    for target in targetSeq:
+        if not(target in compareSeq):
+            output.add(target)
+    return output
 
 proc getFileNameWithExt*(targetPath: string): string =
-  ## get file name with ext from path
-  var (_, file, ext) = splitFile(targetPath)
-  file &= ext
-  return file
+    ## get file name with ext from path
+    var (_, file, ext) = splitFile(targetPath)
+    file &= ext
+    return file
 
 proc getTargetExtFileLists*(targetDirPath: string, targetExt: string): seq[string] =
-  ## extract yml file name seq to specified directory path
-  var r: seq[string] = @[]
-  for f in walkDirRec(targetDirPath):
-    if f.endsWith(targetExt):
-      r.insert(getFileNameWithExt(f))
-  # removed duplicated file name from seq
-  r = deduplicate(r)
-  if r.len() == 0:
-    quit("Target file does not exist in specified directory. Please check your option parameters.")
-  else:
-    r
+    ## extract yml file name seq to specified directory path
+    var r: seq[string] = @[]
+    for f in walkDirRec(targetDirPath):
+        if f.endsWith(targetExt):
+            r.insert(getFileNameWithExt(f))
+    # removed duplicated file name from seq
+    r = deduplicate(r)
+    if r.len() == 0:
+        quit("Target file does not exist in specified directory. Please check your option parameters.")
+    else:
+        r
 
 
 proc getHayabusaCsvData*(csvPath: string, columnName: string): Tableref[string,
-    seq[string]] =
-  ## procedure for Hayabusa output csv read data.
+        seq[string]] =
+    ## procedure for Hayabusa output csv read data.
 
-  var s = newFileStream(csvPath, fmRead)
-  # if csvPath is not valid, error output and quit.
-  if s == nil:
-    quit("Cannot open the file. Please check that the file format is CSV. FilePath: " & csvPath)
+    var s = newFileStream(csvPath, fmRead)
+    # if csvPath is not valid, error output and quit.
+    if s == nil:
+        quit("Cannot open the file. Please check that the file format is CSV. FilePath: " & csvPath)
 
-  # parse csv
-  var p: CsvParser
-  open(p, s, csvPath)
-  p.readHeaderRow()
+    # parse csv
+    var p: CsvParser
+    open(p, s, csvPath)
+    p.readHeaderRow()
 
-  let r = newTable[string, seq[string]]()
-  # initialize table
-  for h in items(p.headers):
-    r[h] = @[]
-
-  # insert csv data to table
-  while p.readRow():
+    let r = newTable[string, seq[string]]()
+    # initialize table
     for h in items(p.headers):
-      r[h].add(p.rowEntry(h))
-  if not r.contains(columnName):
-    quit(fmt"Coud not find the {column_name} column. Please run hayabusa with the verbose profile (-P option).")
+        r[h] = @[]
 
-  return r
+    # insert csv data to table
+    while p.readRow():
+        for h in items(p.headers):
+            r[h].add(p.rowEntry(h))
+    if not r.contains(columnName):
+        quit(fmt"Coud not find the {column_name} column. Please run hayabusa with the verbose profile (-p option).")
+
+    return r
