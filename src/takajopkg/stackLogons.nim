92027f84fdf9155e6a0f4c15b1d266421c411813
Yamato Security <71482215+YamatoSecurity@users.noreply.github.com>
2023-07-23 08:28:54 +0900

    stack-logons working

--- src/takajopkg/stackLogons.nim
+++ src/takajopkg/stackLogons.nim
@@ -2,6 +2,7 @@
 # Output to stdout in tables (Target User, Target Computer, Logon Type, Source Computer)
 # Save to CSV
 # Remove local logins
+# Output to screen in tables
 
 proc stackLogons(output: string = "", quiet: bool = false, timeline: string) =
     let startTime = epochTime()
@@ -22,45 +23,32 @@ proc stackLogons(output: string = "", quiet: bool = false, timeline: string) =
         uniqueResults = initTable[string, tuple[count: int, tgtUser: string, tgtComp: string, logonType: string, srcIP: string, srcComp: string]]()  # tuple for unique counts
     bar.start()
 
+    # Loop through JSON lines
     for line in lines(timeline):
         bar.increment()
         let jsonLine = parseJson(line)
         let ruleTitle = jsonLine["RuleTitle"].getStr()
 
-        #EID 4624 Successful Logon
+        # EID 4624 Successful Logon
         if isEID_4624(ruleTitle) == true:
             inc EID_4624_count
             var singleResultTable = newTable[string, string]()
-            #let details = jsonLine["Details"]
-            #let extraFieldInfo = jsonLine["ExtraFieldInfo"]
-            #let logonType = details.extractInt("Type")
 
+            #change to just strings?
             singleResultTable["TgtUser"] = getJsonValue(jsonLine, @["Details", "TgtUser"])
             singleResultTable["TgtComp"] = getJsonValue(jsonLine, @["Computer"])
             singleResultTable["LogonType"] = logonNumberToString(parseInt(getJsonValue(jsonLine, @["Details", "Type"])))
             singleResultTable["SrcIP"] = getJsonValue(jsonLine, @["Details", "SrcIP"])
             singleResultTable["SrcComp"] = getJsonValue(jsonLine, @["Details", "SrcComp"])
 
-            # Combine all fields into a unique string and update counts
-            #[
-            let combinedKey = $singleResultTable
-            if combinedKey in uniqueResults:
-                uniqueResults[combinedKey].count.inc()
-            else:
-                uniqueResults[combinedKey] = (count: 1, tgtUser: singleResultTable["TgtUser"], tgtComp: singleResultTable["TgtComp"], logonType: singleResultTable["LogonType"], srcIP: singleResultTable["SrcIP"], srcComp: singleResultTable["SrcComp"])
-]#
-            seqOfStrings.add(singleResultTable["TgtUser"] & "🦅" & singleResultTable["TgtComp"] & singleResultTable["LogonType"] & "🦅" & singleResultTable["SrcIP"] & "🦅" & singleResultTable["SrcComp"])
-            seqOfResultsTables.add(singleResultTable)
-
+            # Create a combination of TgtUser,TgtComp,LogonType,SrcIP,SrcComp
+            seqOfStrings.add(singleResultTable["TgtUser"] & "," & singleResultTable["TgtComp"] & "," & singleResultTable["LogonType"] & "," & singleResultTable["SrcIP"] & "," & singleResultTable["SrcComp"])
     bar.finish()
     echo ""
-    #seqOfStrings.sort()
-    # Convert table to a sequence of tuples and sort by count
-    #let sortedResults = toSeq(uniqueResults.pairs).sortedBy(it[1].count)
-
 
     var countsTable: Table[string, int] = initTable[string, int]()
 
+    # Add a count for each time the unique string was found
     for string in seqOfStrings:
         if not countsTable.hasKey(string):
             countsTable[string] = 0
@@ -74,24 +62,23 @@ proc stackLogons(output: string = "", quiet: bool = false, timeline: string) =
     # Sort the sequence in descending order based on the count
     seqOfPairs.sort(proc (x, y: (string, int)): int = y[1] - x[1])
 
-    # Print the sorted counts with unique strings
-    for (string, count) in seqOfPairs:
-        echo count, string
+    # Print results to screen
+    if output == "":
+        # Print the sorted counts with unique strings
+        for (string, count) in seqOfPairs:
+            var commaDelimitedStr = $count & "," & string
+            commaDelimitedStr = replace(commaDelimitedStr, ",", " | ")
+            echo commaDelimitedStr
+    # Save to CSV file
+    else:
+        let file = open(output, fmWrite)
+        # Write headers
+        writeLine(file, "Count,TgtUser,TgtComp,LogonType,SrcIP,SrcComp")
 
-
-    # Write results to CSV
-   # let file = open(output, fmWrite)
-   # defer: close(file)
-
-    # Write headers
-   #writeLine(file, "Count,TgtUser,TgtComp,LogonType,SrcIP,SrcComp")
-
-    # Write results
-    #[
-    for result in sortedResults:
-        let entry = result[1]
-        writeLine(file, fmt"{entry.count},{entry.tgtUser},{entry.tgtComp},{entry.logonType},{entry.srcIP},{entry.srcComp}")
-]#
+        # Write results
+        for (string, count) in seqOfPairs:
+            writeLine(file, $count & "," & string)
+        close(file)
 
     let endTime = epochTime()
     let elapsedTime2 = int(endTime - startTime)
