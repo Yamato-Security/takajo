010958cbf944465e57b2c26fdbd9eb1bed95ea79
fukusuket <41001169+fukusuket@users.noreply.github.com>
2023-10-12 00:36:52 +0900

    feat: add elapsed time.

--- src/takajopkg/sysmonProcessTree.nim
+++ src/takajopkg/sysmonProcessTree.nim
@@ -89,7 +89,7 @@ proc createProcessObj(jsonLine:JsonNode): processObject =
 proc sysmonProcessTree(output: string = "", processGuid: string,
         quiet: bool = false, timeline: string) =
     ## Procedure for displaying Sysmon's process tree
-
+    let startTime = epochTime()
     if not quiet:
         # Display the logo
         styledEcho(fgGreen, outputLogo())
@@ -183,7 +183,6 @@ proc sysmonProcessTree(output: string = "", processGuid: string,
         else:
             moveProcessObjectToChild(stockedProcObjTbl[pguid], stockedProcObjTbl[processGuid], outProcObjTbl[processGuid])
 
-
     # Display process tree for the specified process root
     var outStrSeq: seq[string] = @[]
     if parentsKey != "":
@@ -205,3 +204,13 @@ proc sysmonProcessTree(output: string = "", processGuid: string,
                 styledEcho(fgGreen, line)
             else:
                 echo line
+        echo ""
+
+    let endTime = epochTime()
+    let elapsedTime = int(endTime - startTime)
+    let hours = elapsedTime div 3600
+    let minutes = (elapsedTime mod 3600) div 60
+    let seconds = elapsedTime mod 60
+
+    echo "Elapsed time: ", $hours & " hours, " & $minutes & " minutes, " & $seconds & " seconds"
+    echo ""
