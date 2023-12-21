1062ed91432a27070a8e824d409775d765f93fb4
fukusuket <41001169+fukusuket@users.noreply.github.com>
2023-12-21 09:32:41 +0900

    feat: add ttp-summary command

---
+++ src/takajopkg/ttpSummary.nim
@@ -0,0 +1,136 @@
+proc readJsonFromFile(filename: string): JsonNode =
+  var
+    file: File
+    content: string
+  if file.open(filename):
+    content = file.readAll()
+    file.close()
+    result = parseJson(content)
+
+proc compareArrays(a, b: array[4, string]): int =
+  for i in 0..<4:
+    if a[i] < b[i]:
+      return -1
+    elif a[i] > b[i]:
+      return 1
+  return 0
+
+proc ttpSummary(output: string = "", quiet: bool = false, timeline: string) =
+    let startTime = epochTime()
+    if not quiet:
+        styledEcho(fgGreen, outputLogo())
+
+    if not os.fileExists(timeline):
+        echo "The file '" & timeline & "' does not exist. Please specify a valid file path."
+        quit(1)
+
+    if not os.fileExists("mitre-attack.json"):
+        echo "The file '" & "mitre-attack.json" & "' does not exist. Please specify a valid file path."
+        quit(1)
+
+    if not isJsonConvertible(timeline):
+        quit(1)
+
+    echo "Started the TTP Summary command."
+    echo "This command outputs TTP summary."
+
+    echo ""
+    echo "Counting total lines. Please wait."
+    let totalLines = countLinesInTimeline(timeline)
+    echo "Total lines: ", totalLines
+    echo ""
+    let tac_no = {
+                        "Reconnaissance": "01. ",
+                        "Resource Development": "02. ",
+                        "Initial Access": "03. ",
+                        "Execution": "04. ",
+                        "Persistence": "05. ",
+                        "Privilege Escalation": "06. ",
+                        "Defense Evasion": "07. ",
+                        "Credential Access": "08. ",
+                        "Discovery": "09. ",
+                        "Lateral Movement": "10. ",
+                        "Collection": "11. ",
+                        "Exfiltration": "12. ",
+                        "Command and Control": "13. ",
+                        "Impact": "14. "
+                    }.toTable
+    let attack = readJsonFromFile("mitre-attack.json")
+    var
+        bar: SuruBar = initSuruBar()
+        seqOfResultsTables: seq[array[4, string]]
+
+    bar[0].total = totalLines
+    bar.setup()
+
+    for line in lines(timeline):
+        inc bar
+        bar.update(1000000000) # refresh every second
+        let jsonLine = parseJson(line)
+        try:
+            let com = jsonLine["Computer"].getStr()
+            for tag in jsonLine["MitreTags"]:
+                if tag.getStr() notin attack:
+                    continue
+                let res = attack[tag.getStr()]
+                let dat = res["Tactic"].getStr()
+                let tac = tac_no[dat] & dat
+                let tec = res["Technique"].getStr()
+                let sub = res["Sub-Technique"].getStr()
+                seqOfResultsTables.add([com, tac, tec, sub])
+        except CatchableError:
+            continue
+    seqOfResultsTables.sort(compareArrays)
+    bar.finish()
+
+    let header = ["Computer", "Tactic", "Technique", "Sub-Technique", "Count"]
+    var prev = ["","","",""]
+    var count = 1
+    if output != "":
+        # Open file to save results
+        var outputFile = open(output, fmWrite)
+
+        ## Write CSV header
+        outputFile.write(header.join(",") & "\p")
+
+        ## Write contents
+        for arr in seqOfResultsTables:
+            if arr == prev:
+                count += 1
+                continue
+            for i, val in enumerate(arr):
+                outputFile.write(escapeCsvField(val) & ",")
+            outputFile.write(escapeCsvField(intToStr(count)))
+            prev = arr
+            count = 1
+            outputFile.write("\p")
+        outputFile.close()
+        let fileSize = getFileSize(output)
+        echo ""
+        echo "Saved results to " & output & " (" & formatFileSize(fileSize) & ")"
+    else:
+        echo ""
+        var table: TerminalTable
+        table.add header
+        for arr in seqOfResultsTables:
+            if arr == prev:
+                count += 1
+                continue
+            table.add arr[0], arr[1], arr[2], arr[3], intToStr(count)
+            prev = arr
+            count = 1
+        table.echoTableSepsWithStyled(seps = boxSeps)
+
+    echo ""
+    if seqOfResultsTables.len == 0:
+        echo "No MITRE ATT&CK tags were found in the Hayabusa results."
+        echo "Please run your Hayabusa scan with a profile that includes the %MitreTags% field. (ex: -p verbose)"
+
+    let endTime = epochTime()
+    let elapsedTime = int(endTime - startTime)
+    let hours = elapsedTime div 3600
+    let minutes = (elapsedTime mod 3600) div 60
+    let seconds = elapsedTime mod 60
+    echo ""
+    echo "Elapsed time: ", $hours & " hours, " & $minutes & " minutes, " & $seconds & " seconds"
+    echo ""
