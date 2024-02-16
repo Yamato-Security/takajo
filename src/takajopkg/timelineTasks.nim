95cb513a4577b97f8f7a22d98bbec56f770a1317
fukusuket <41001169+fukusuket@users.noreply.github.com>
2024-02-16 22:53:24 +0900

    feat: iterate all xml tag

--- src/takajopkg/timelineTasks.nim
+++ src/takajopkg/timelineTasks.nim
@@ -5,6 +5,16 @@ proc extractText(allXmlNodes:XmlNode, tag:string): string =
         return decodeEntity(xml.replace("<" & tag & ">", "").replace("</" & tag & ">",""))
     return ""
 
+proc walkXml(node:XmlNode, tagName: string, resuls: var OrderedTable[string, string]) =
+  if node.len == 0 and node.innerText.len != 0:
+      resuls[tagName] = node.innerText
+      return
+  for child in node:
+      if tagName != tag(node):
+          walkXml(child, tagName & ":" & tag(node), resuls)
+      else:
+          walkXml(child, tag(node), resuls)
+
 proc timelineTasks(output: string, outputLogoffEvents: bool = false, quiet: bool = false, timeline: string) =
     let startTime = epochTime()
     if not quiet:
@@ -17,9 +27,9 @@ proc timelineTasks(output: string, outputLogoffEvents: bool = false, quiet: bool
     if not isJsonConvertible(timeline):
             quit(1)
 
-    echo "Started the Timeline Logon command"
+    echo "Started the Timeline Tasks command"
     echo ""
-    echo "This command creates a CSV timeline of scheduled tasks."
+    echo "This command creates a CSV timeline of scheduled task events."
     echo ""
 
     echo "Counting total lines. Please wait."
@@ -28,12 +38,12 @@ proc timelineTasks(output: string, outputLogoffEvents: bool = false, quiet: bool
     echo "Total lines: ", totalLines
     echo ""
 
-    echo "Creating a logon timeline. Please wait."
+    echo "Creating a scheduled tasks timeline. Please wait."
     echo ""
 
     var
         bar: SuruBar = initSuruBar()
-        results = newSeq[array[9, string]]()
+        rowData = newSeq[(OrderedTable[string, string], OrderedTable[string, string])]()
 
     bar[0].total = totalLines
     bar.setup()
@@ -52,34 +62,68 @@ proc timelineTasks(output: string, outputLogoffEvents: bool = false, quiet: bool
             let subjectDomainName = jsonLine["ExtraFieldInfo"]["SubjectDomainName"].getStr("N/A")
             let subjectLogonId = jsonLine["Details"]["LID"].getStr("N/A")
             let content = jsonLine["Details"]["Content"].getStr("N/A").replace("\\r\\n", "")
-            let node = parseXml(content)
-            let commands = extractText(node, "Command")
-            let arguments = extractText(node, "Arguments")
-            results.add([ts, subjectUserName, taskName, commands, arguments, subjectUserSid, subjectDomainName, subjectLogonId, content])
+            var basicTable = initOrderedTable[string, string]()
+            basicTable["Timestamp"] = ts
+            basicTable["TaskName"] = taskName
+            basicTable["User"] = subjectUserName
+            basicTable["SubjectUserSid"] = subjectUserSid
+            basicTable["SubjectDomainName"] = subjectDomainName
+            basicTable["SubjectLogonId"] = subjectLogonId
+            var detailedTable = initOrderedTable[string, string]()
+            try:
+                let rootNode = parseXml(content)
+                basicTable["Command"] = extractText(rootNode, "Command")
+                basicTable["Arguments"] = extractText(rootNode, "Arguments")
+                for tagName in ["Triggers", "Principal", "Settings", "RegistrationInfo"]:
+                    for i, node in enumerate(rootNode.findAll(tagName)):
+                        if i == 0:
+                            walkXml(node, tagName , detailedTable)
+                        else:
+                            walkXml(node, tagName & "[" & intToStr(i) & "]", detailedTable)
+            except XmlError:
+                continue
+            rowData.add((basicTable, detailedTable))
 
     bar.finish()
 
+    if rowData.len == 0:
+        echo ""
+        echo "No scheduled task events were found."
+        echo ""
+    else:
+        var allDetailedKeys = initOrderedSet[string]()
+        for (_, detailedTable) in rowData:
+            for key in detailedTable.keys:
+                allDetailedKeys.incl(key)
+        let basicHeader = @["Timestamp", "TaskName", "User", "SubjectUserSid", "SubjectDomainName", "SubjectLogonId", "Command", "Arguments"]
+        let detailedHeader = toSeq(allDetailedKeys)
 
-    # Save results
-    var outputFile = open(output, fmWrite)
-    let header = ["Timestamp", "SubjectUserName", "TaskName", "Command", "Arguments","SubjectUserSid", "SubjectDomainName", "SubjectLogonId", "TaskContent"]
+        # Save results
+        var outputFile = open(output, fmWrite)
 
-    ## Write CSV header
-    for h in header:
-        outputFile.write(h & ",")
-    outputFile.write("\p")
-
-    ## Write contents
-    for row in results:
-        for column in row:
-            outputFile.write(escapeCsvField(column) & ",")
+        ## Write CSV header
+        let header = concat(basicHeader, detailedHeader)
+        for h in header:
+            outputFile.write(h & ",")
         outputFile.write("\p")
-    let fileSize = getFileSize(output)
-    outputFile.close()
 
-    echo ""
-    echo "Saved results to " & output & " (" & formatFileSize(fileSize) & ")"
-    echo ""
+        ## Write contents
+        for (basicTable, detailedTable) in rowData:
+            for i, columnName in enumerate(header):
+                if columnName in basicHeader:
+                    outputFile.write(basicTable[columnName] & ",")
+                elif i > basicHeader.len - 1:
+                    if columnName in detailedTable:
+                        outputFile.write(escapeCsvField(detailedTable[columnName]) & ",")
+                    else:
+                        outputFile.write(",")
+            outputFile.write("\p")
+        outputFile.close()
+        let fileSize = getFileSize(output)
+
+        echo ""
+        echo "Saved results to " & output & " (" & formatFileSize(fileSize) & ")"
+        echo ""
 
     let endTime = epochTime()
     let elapsedTime2 = int(endTime - startTime)
