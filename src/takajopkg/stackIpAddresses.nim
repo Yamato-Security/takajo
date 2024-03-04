69dd9cc0eeda7f53d3424c2be77269a5c5a8a67f
fukusuket <41001169+fukusuket@users.noreply.github.com>
2024-03-04 09:50:23 +0900

    chg: exclude 127.0.0.1 and ::1

--- src/takajopkg/stackIpAddresses.nim
+++ src/takajopkg/stackIpAddresses.nim
@@ -3,7 +3,9 @@ proc stackIpAddresses(level: string = "informational", targetIpAddresses: bool =
     checkArgs(quiet, timeline, level)
     let totalLines = countJsonlAndStartMsg("IpAddresses", "the IpAddress field as well as show alert information", timeline)
     let key = if targetIpAddresses: "TgtIP" else: "SrcIP"
-    let eventFilter = proc(x: HayabusaJson): bool = true
+    let eventFilter = proc(x: HayabusaJson): bool =
+        let ip = getJsonValue(x.Details, @[key])
+        return ip != "127.0.0.1" and ip != "::1"
     let getStackKey = proc(x: HayabusaJson): (string, seq[string]) = (getJsonValue(x.Details, @[key]), @[])
     let stack = processJSONL(eventFilter, getStackKey, totalLines, timeline, level)
     outputResult(output, "IpAddress", stack, @[], isMinColumns=true)
