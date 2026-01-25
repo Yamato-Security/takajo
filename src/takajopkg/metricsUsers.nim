81a9cb302693cb1666eb0f79a9fb761f9cb0ad3b
fukusuket <41001169+fukusuket@users.noreply.github.com>
2026-01-25 09:42:11 +0900

    feat: enhance metricsUsersCmd to track successful and failed logon sources and hostnames

--- src/takajopkg/metricsUsers.nim
+++ src/takajopkg/metricsUsers.nim
@@ -2,10 +2,14 @@ const metricsUsersMsg = "This command creates user metrics from logon events."
 
 type
   metricsUsersCmd* = ref object of AbstractCmd
-    header* = @["Target Computer", "User", "SID", "Logon Count", "Source"]
+    header* = @["Target Computer", "User", "SID", "Successful Logon Count", "Successful Sources", "Successful Hostnames", "Failed Logon Count", "Failed Sources", "Failed Hostnames"]
     count*: CountTable[string] = initCountTable[string]()
+    failedCount*: CountTable[string] = initCountTable[string]()
     userSidMap*: Table[string, string] = initTable[string, string]()
-    userSourceMap*: Table[string, HashSet[string]] = initTable[string, HashSet[string]]()
+    userSuccessSrcIPMap*: Table[string, HashSet[string]] = initTable[string, HashSet[string]]()
+    userFailedSrcIPMap*: Table[string, HashSet[string]] = initTable[string, HashSet[string]]()
+    userSuccessHostMap*: Table[string, HashSet[string]] = initTable[string, HashSet[string]]()
+    userFailedHostMap*: Table[string, HashSet[string]] = initTable[string, HashSet[string]]()
 
 proc getDetailValue(details: JsonNode, key: string): string =
   if details.hasKey(key):
@@ -28,25 +32,75 @@ method analyze*(self: metricsUsersCmd, x: HayabusaJson) =
   var user = getDetailValue(x.Details, "TgtUser")
   if user == "N/A":
     user = getDetailValue(x.Details, "User")
-  var source = "N/A"
-  if eventID == 4624 or eventID == 4625:
-    source = getDetailValue(x.Details, "SrcComp") & " ¦ " & getDetailValue(x.Details, "SrcIP")
-  elif eventID == 4648:
-    source = getDetailValue(x.Details, "SrcIP")
-  elif eventID == 21 or eventID == 23 or eventID == 302 or eventID == 303:
-    source = getDetailValue(x.Details, "SrcIP")
+  var sourceIP = "N/A"
+  var sourceComp = "N/A"
+  var sourceFailIP = "N/A"
+  var sourceFailComp = "N/A"
   let key = computer & "|" & user
-  self.count.inc(key)
+  if eventID == 4625:
+    sourceFailIP = getDetailValue(x.Details, "SrcIP")
+    sourceFailComp = getDetailValue(x.Details, "SrcComp")
+    self.failedCount.inc(key)
+  else:
+    sourceIP = getDetailValue(x.Details, "SrcIP")
+    if eventID == 4624:
+      sourceComp = getDetailValue(x.Details, "SrcComp")
+    if eventID == 21 or eventID == 303 or eventID == 4624 or eventID == 4648:
+        self.count.inc(key)
   if not self.userSidMap.hasKey(key) or (self.userSidMap[key] == "N/A" and sid != "N/A"):
     self.userSidMap[key] = sid
-  if not self.userSourceMap.hasKey(key):
-    self.userSourceMap[key] = initHashSet[string]()
-  if source != "N/A" and source != "-":
-    let sources = source.split(" ¦ ")
+  if not self.userSuccessSrcIPMap.hasKey(key):
+    self.userSuccessSrcIPMap[key] = initHashSet[string]()
+  if not self.userFailedSrcIPMap.hasKey(key):
+    self.userFailedSrcIPMap[key] = initHashSet[string]()
+  if not self.userSuccessHostMap.hasKey(key):
+    self.userSuccessHostMap[key] = initHashSet[string]()
+  if not self.userFailedHostMap.hasKey(key):
+    self.userFailedHostMap[key] = initHashSet[string]()
+  
+  if sourceIP != "N/A" and sourceIP != "-":
+    let sources = sourceIP.split(" ¦ ")
     for src in sources:
       if src != "" and src != "-":
-        self.userSourceMap[key].incl(src)
+        self.userSuccessSrcIPMap[key].incl(src)
 
+  if sourceFailIP != "N/A" and sourceFailIP != "-":
+    let sources = sourceFailIP.split(" ¦ ")
+    for src in sources:
+      if src != "" and src != "-":
+        self.userFailedSrcIPMap[key].incl(src)
+  
+  if sourceComp != "N/A" and sourceComp != "-":
+    let sources = sourceComp.split(" ¦ ")
+    for src in sources:
+      if src != "" and src != "-":
+        self.userSuccessHostMap[key].incl(src)
+ 
+  if sourceFailComp != "N/A" and sourceFailComp != "-":
+    let sources = sourceFailComp.split(" ¦ ")
+    for src in sources:
+      if src != "" and src != "-":
+        self.userFailedHostMap[key].incl(src)
+
+proc buildColumns(key: string, count: int, self: metricsUsersCmd): seq[string] =
+    let computer = key.split("|")[0]
+    let user = key.split("|")[1]
+    var sid = "N/A"
+    if self.userSidMap.hasKey(key):
+        sid = self.userSidMap[key]
+    var source = "N/A"
+    var sourceFail = "N/A"
+    var sourceComp = "N/A"
+    var sourceFailComp = "N/A"
+    if self.userSuccessSrcIPMap.hasKey(key):
+        source = self.userSuccessSrcIPMap[key].toSeq().join(" ¦ ")
+    if self.userFailedSrcIPMap.hasKey(key):
+        sourceFail = self.userFailedSrcIPMap[key].toSeq().join(" ¦ ")
+    if self.userSuccessHostMap.hasKey(key):
+        sourceComp = self.userSuccessHostMap[key].toSeq().join(" ¦ ")
+    if self.userFailedHostMap.hasKey(key):
+        sourceFailComp = self.userFailedHostMap[key].toSeq().join(" ¦ ")
+    return @[computer, user, sid, intToStr(self.count.getOrDefault(key, 0)).insertSep(','), source, sourceComp, intToStr(self.failedCount.getOrDefault(key, 0)).insertSep(','), sourceFail, sourceFailComp]
 
 method resultOutput*(self: metricsUsersCmd) =
     var savedFiles = "n/a"
@@ -66,15 +120,7 @@ method resultOutput*(self: metricsUsersCmd) =
             let outputFile = open(self.output, fmWrite)
             writeLine(outputFile, self.header.join(","))
             for (key, count) in sortedCounts:
-                let computer = key.split("|")[0]
-                let user = key.split("|")[1]
-                var sid = "N/A"
-                if self.userSidMap.hasKey(key):
-                    sid = self.userSidMap[key]
-                var source = "N/A"
-                if self.userSourceMap.hasKey(key):
-                    source = self.userSourceMap[key].toSeq().sorted().join(" ¦ ")
-                let columns = @[computer, user, sid, intToStr(count).insertSep(','), source]
+                let columns = buildColumns(key, count, self)
                 for i, key in columns:
                     if i < columns.len - 1:
                         outputFile.write(escapeCsvField(columns[i]) & ",")
@@ -91,15 +137,8 @@ method resultOutput*(self: metricsUsersCmd) =
         var table: TerminalTable
         table.add self.header
         for (key, count) in sortedCounts:
-            let computer = key.split("|")[0]
-            let user = key.split("|")[1]
-            var sid = "N/A"
-            if self.userSidMap.hasKey(key):
-                sid = self.userSidMap[key]
-            var source = "N/A"
-            if self.userSourceMap.hasKey(key):
-                source = self.userSourceMap[key].toSeq().join(" ¦ ")
-            table.add computer, user, sid, intToStr(count).insertSep(','), source
+            let col = buildColumns(key, count, self)
+            table.add col[0], col[1], col[2], col[3], col[4], col[5], col[6], col[7], col[8]
         if self.displayTable:
           echo ""
           table.echoTableSepsWithStyled(seps = boxSeps)
